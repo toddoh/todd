0018d0dd1f6aeb9ca14fe547c9955617c28e86ac
Todd Stonefield-Oh <toddstonefieldoh@gmail.com>
2023-01-22 14:13:56 -0500

    fix font import

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ function Home({
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <GlobalStyle />
